947497bf98c96a9491f2610fb17a1ca93da7f2be
tomo4ka10 <katotomoyojb2@gmail.com>
2025-11-18 10:06:29 +0900

    20251118_関数化

--- ttest_2.py
+++ ttest_2.py
@@ -5,30 +5,42 @@ import matplotlib.pyplot as plt
 # 独立した2標本のt検定において有意水準や検定力，効果量からサンプルサイズを決定するコード
 # サンプルサイズ決定について学習する目的
 
-# 両側t検定
+
+def cal_ttest2_ss(alpha, power_want, d, n1_n2, tail_test):
+    # 等分散の仮定
+    # サンプルサイズ探索
+    n1 = 2  # 最小値からスタート
+    while True:
+        n2 = int(round(n1 * n1_n2)) # n2を比率から求める
+        df = n1 + n2 - 2 # 自由度
+        delta = d * np.sqrt(n1 * n2 / (n1 + n2)) # 非心パラメータ
+        t_a2 = t.ppf(1-alpha/2, df) # 上側alpha/2点
+
+        # 両側or片側検定
+        match tail_test:
+            case 'two-tailed': # 両側検定
+                power = 1 - nct.cdf(t_a2, df, delta) + nct.cdf(-t_a2, df, delta)
+            case 'upper-one-tailed': # 上側片側検定
+                power = 1 - nct.cdf(t_a2, df, delta)
+
+        # 検出力が設定した検定力より大きくなるまで
+        if power >= power_want:
+            break
+        n1 += 1
+    
+    return n1, n2, df, delta, t_a2, power
 
 # 指定する値
 alpha = 0.05 # 有意水準
 power_want = 0.8 # 指定する検定力 
 d = 0.8 # 効果量
 n1_n2 = 1 # 2群のサンプルサイズの比率(n1/n2)
+tail_test = 'upper-one-tailed'
 
+[n1, n2, df, delta, t_a2, power] = cal_ttest2_ss(alpha, power_want, d, n1_n2, tail_test)
+print(f"n1={n1}, n2={n2}, Power={power:.4f}, {t_a2}")
 
-# サンプルサイズ探索
-n1 = 2  # 最小値からスタート
-while True:
-    n2 = int(round(n1 * n1_n2))
-    df = n1 + n2 - 2 # 自由度
-    delta = d * np.sqrt(n1 * n2 / (n1 + n2)) # 非心パラメータ
-    t_a2 = t.ppf(1-alpha/2, df) # 上側alpha/2点
-    # 両側検定のPower
-    power = 1 - nct.cdf(t_a2, df, delta) + nct.cdf(-t_a2, df, delta)
-    if power >= power_want:
-        break
-    n1 += 1
-
-print(f"n1={n1}, n2={n2}, Power={power:.4f}")
-
+'''
 # figure：t分布と非心t分布，有意水準，上側alpha/2点など
 x = np.linspace(-4,8,100)
 plt.plot(x,t.pdf(x,df),color='red',label='t') # t分布，帰無仮説H0
@@ -52,3 +64,5 @@ plt.ylabel('P(x)')
 plt.legend()
 plt.title(f"df={df}, t_alpha/2={np.round(t_a2,3)}, (n1,n2)={n1,n2}, d={d}")
 plt.show()
+print(f"n1={n1}, n2={n2}, Power={power:.4f}")
+'''
